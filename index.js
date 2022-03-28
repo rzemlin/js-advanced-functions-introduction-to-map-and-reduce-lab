2caf2c868fde9f7186ce3ca0489baf99ae5004f8
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-03-27 21:11:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(thing) {
+    let r = []
+    for (let i = 0; i < thing.length; i++ ) {
+      r.push(-1 * thing[i])
+    }
+    return r
+  }
+
+  function mapToNoChange(thing) {
+    let r = []
+    for (let i = 0; i < thing.length; i++ ) {
+      r.push(thing[i])
+    }
+    return r
+  }
+
+  function mapToDouble(thing) {
+    let r = []
+    for (let i = 0; i < thing.length; i++ ) {
+      r.push(2 * thing[i])
+    }
+    return r
+  }
+  
+  function mapToSquare(thing) {
+    let r = []
+    for (let i = 0; i < thing.length; i++ ) {
+      r.push(thing[i] ** 2)
+    }
+    return r
+  }
+  
+  function reduceToTotal(thing, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < thing.length; i++ ) {
+      total = total + thing[i]
+    }
+    return total
+  }
+
+  function reduceToAllTrue(thing) {
+    for (let i = 0; i < thing.length; i++ ) {
+      if (!thing[i]) return false
+    }
+    return true
+  }
+
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
+  
